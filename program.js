5244cc78b80c5d8b1550db502f69c0db992f46f4
Adin Handukic <adiin.handukic@gmail.com>
2022-09-28 16:03:04 +0200

    28.09.22 | 16:02

--- program.js
+++ program.js
@@ -3,8 +3,10 @@ function addtodo(){
     let table = document.getElementById("table"),
     newRow = table.insertRow(table.length),
     cell1 = newRow.insertCell(0),
+    cell2=newRow.insertCell(1),
     todo = document.getElementById("todo").value;
     cell1.innerHTML=todo;
+    cell2.innerHTML="done"
 }
 }
 function checkEmptyInput()
@@ -17,4 +19,23 @@ function checkEmptyInput()
         isEmpty = true;
 }
 return isEmpty;
+}
+
+let Index,
+    table = document.getElementById('table');
+
+
+function removing() {
+    for(let i = 1; i < table.rows.length; i++)
+{
+table.rows[i].cells[1].onclick = function()
+{
+    let c = confirm("u sure?");
+    if(c === true)
+    {
+        Index = this.parentElement.rowIndex;
+        table.deleteRow(Index);
+    }
+}
+}
 }
