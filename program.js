552628137bdd967d2cb36d488d5fce0b78356c0c
Adin Handukic <adiin.handukic@gmail.com>
2022-09-28 14:40:58 +0200

    Final Version 28.09.2022

--- program.js
+++ program.js
@@ -1,7 +1,20 @@
 function addtodo(){
+    if(!checkEmptyInput()){
     let table = document.getElementById("table"),
     newRow = table.insertRow(table.length),
     cell1 = newRow.insertCell(0),
     todo = document.getElementById("todo").value;
     cell1.innerHTML=todo;
+}
+}
+function checkEmptyInput()
+{
+    let isEmpty = false,
+    todo = document.getElementById("todo").value;
+
+    if(todo === ""){
+        alert("You have nothing to do?");
+        isEmpty = true;
+}
+return isEmpty;
 }
